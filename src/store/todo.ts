36a33be278f0b7b4572524b8f8b215a26b99ffe8
zfeny <zfeny123@gmail.com>
2023-08-23 10:34:39 +0800

    底部栏/待办修改

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -1,66 +1,16 @@
 const defaultLongTodoList = [
   {
     id: "qt-SUQNbOKK4n4Zn8izTV",
-    title: "在7个月内学完MIT数学基础课程",
+    title: "在这里输入长期计划",
     createTime: 1643096714078,
-  },
-  {
-    id: "nqw-TNX8L32b6Dkk8izkC",
-    title: "每天上午六点半起床坚持写博客",
-    createTime: 1643096677407,
-  },
-  {
-    id: "9qeGBwEtc74QZR97HjxoO",
-    title: "王者打到星耀段位",
-    createTime: 1643094950696,
-  },
-  {
-    id: "0UvstY435GXVLVhFPl5jr",
-    title: "每天至少去健身房锻炼1小时",
-    createTime: 1643094738177,
-  },
-  {
-    id: "0UvstY435GXVLVhFPl6er",
-    title: "读完Vue的源码分析",
-    createTime: 1643094738177,
   },
 ];
 
 const defaultShortTodoList = [
   {
     id: "0UvstY435GXVLVhFPl6er",
-    title: "网站正式版本启动",
+    title: "在这里输入短期计划",
     createTime: 1643094738177,
-  },
-  {
-    id: "Ixq4XYjhkASrHcuUy4f6k",
-    title: "去跑步三十分钟 * 3次",
-    createTime: 1643096402279,
-  },
-  {
-    id: "Io2SP9DoIBFZyW3OgiBNQ",
-    title: "追漫威最近的电影 * 3部",
-    createTime: 1643096385839,
-  },
-  {
-    id: "Nx72Sq9RWOxyfbG7uNEjt",
-    title: "复习飞桨创造营的课程",
-    createTime: 1643096326151,
-  },
-  {
-    id: "Xyt8D9ZKSJAoAsxN_GfHf",
-    title: "写一个OCR识别网页的开源工具",
-    createTime: 1643096317888,
-  },
-  {
-    id: "IWCtUN0u1FaJdaFy20kQp",
-    title: "nestjs做后端开发",
-    createTime: 1643096300666,
-  },
-  {
-    id: "0UN2W5kTbvBzMn3BYabUI",
-    title: "晚上群里组队打王者",
-    createTime: 1643096241999,
   },
 ];
 
